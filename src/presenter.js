1cc547c895d2d4cc4de1cb97e5db11b5e30f0439
Guillermo <guillermo.maldonado@ucb.edu.bo>
2023-08-27 15:27:39 -0400

    Implementacion de interfaz

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,21 @@
 import sumar from "./sumador";
+import esBisiesto from "./esBisiesto";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const year = document.querySelector("#year");
+
+const form = document.querySelector("#year-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const y = Number.parseInt(year.value);
+if(esBisiesto(y))
+{
+  div.innerHTML = "<p>" + "Es Bisiesto" + "</p>";
+}
+else{
+  div.innerHTML = "<p>" + "No es Bisiesto" + "</p>";
+}
+  
 });
